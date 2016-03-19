4600b7eaf34f4b21d6c37c50f9087ee4678d5b23
nakedible <naked@iki.fi>
2016-03-19 15:10:14 +0200

    Add database operations

--- bot.py
+++ bot.py
@@ -1,6 +1,32 @@
 #!/usr/bin/python3
 
+import json
+
 import irc3
+
+### storage
+
+def read_db(filename):
+    try:
+        with open(filename) as f:
+            return json.load(f)
+    except:
+        import traceback
+        traceback.print_exc()
+        print('failed to read db, starting with empty')
+        return {}
+
+def write_db(filename, val):
+    try:
+        with open(filename, 'w') as f:
+            json.dump(val, f, indent=2)
+            f.write('\n')
+    except:
+        import traceback
+        traceback.print_exc()
+        print('failed to write state file, ignoring error')
+
+### bot
 
 @irc3.plugin
 class MyPlugin:
