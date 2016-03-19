79b5d8b5ba0b39a024800408058df7c668d99115
nakedible <naked@iki.fi>
2016-03-19 14:27:44 +0200

    Add privmsg interception

--- bot.py
+++ bot.py
@@ -2,6 +2,19 @@
 
 import irc3
 
+@irc3.plugin
+class MyPlugin:
+    def __init__(self, bot):
+        self.bot = bot
+        self.log = self.bot.log
+
+    @irc3.event(irc3.rfc.PRIVMSG)
+    def on_privmsg(self, mask=None, data=None, event=None, target=None):
+        print(mask)
+        print(data)
+        print(event)
+        print(target)
+
 def main():
     config = dict(
         nick='furiosaxx', autojoins=['#porno3003'],
@@ -10,6 +23,7 @@ def main():
             'irc3.plugins.core',
             'irc3.plugins.command',
             'irc3.plugins.human',
+            __name__,  # this register MyPlugin
         ]
     )
     bot = irc3.IrcBot.from_config(config)
